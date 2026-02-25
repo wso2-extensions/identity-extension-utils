dc06b06ab0b49ef74066f41207c63663f84a2d33
sadilchamishka <sandilchamishka@gmail.com>
2026-02-25 12:02:45 +0530

    Fix registry based authentication xml file read issue

--- component/helper/src/main/java/org/wso2/carbon/extension/identity/helper/util/IdentityHelperUtil.java
+++ component/helper/src/main/java/org/wso2/carbon/extension/identity/helper/util/IdentityHelperUtil.java
@@ -34,6 +34,7 @@ import org.wso2.carbon.identity.application.authentication.framework.config.mode
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authentication.framework.exception.AuthenticationFailedException;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.registry.core.Registry;
 import org.wso2.carbon.registry.core.Resource;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
@@ -332,11 +333,18 @@ public class IdentityHelperUtil {
                     }
                 }
             }
+            if (Boolean.parseBoolean(IdentityUtil.getProperty(IdentityHelperConstants
+                    .ENABLE_TENANT_AUTHENTICATOR_OVERRIDE_FOR_AUTH_SEQUENCE))) {
+                context.setProperty(IdentityHelperConstants.GET_PROPERTY_FROM_REGISTRY, null);
+            }
         } catch (SAXException | ParserConfigurationException | IOException e) {
             throw new AuthenticationFailedException("Cannot get the parameter values from registry ", e);
         } catch (RegistryException e) {
-            context.setProperty(IdentityHelperConstants.GET_PROPERTY_FROM_REGISTRY,
-                    IdentityHelperConstants.GET_PROPERTY_FROM_REGISTRY);
+            if (!Boolean.parseBoolean(IdentityUtil.getProperty(IdentityHelperConstants
+                    .ENABLE_TENANT_AUTHENTICATOR_OVERRIDE_FOR_AUTH_SEQUENCE))) {
+                context.setProperty(IdentityHelperConstants.GET_PROPERTY_FROM_REGISTRY,
+                        IdentityHelperConstants.GET_PROPERTY_FROM_REGISTRY);
+            }
         } finally {
             PrivilegedCarbonContext.endTenantFlow();
         }
